61e907127c12854ef2d0f7064b019f20a0b8d173
des-yogi <nuevo-design@mail.ru>
2019-05-07 23:30:56 +0300

    Main-nav & metismenu in progress

--- src/blocks/main-nav/main-nav.js
+++ src/blocks/main-nav/main-nav.js
@@ -8,7 +8,7 @@
     function mainNavVisibleToggle(e) {
       e.preventDefault();
       toggler.classList.toggle('burger--close'); // модификатор иконки (должен быть .burger)
-      document.getElementById('main-nav').classList.toggle('main-nav--open');
+      //document.getElementById('main-nav').classList.toggle('main-nav--open');
     }
   }
 
