0801115c72be12f556c8a2adae0fdb4badfd954d
des-yogi <nuevo-design@mail.ru>
2019-05-10 19:13:46 +0300

    Разметка гланой закончена

--- src/blocks/language-select/language-select.js
+++ src/blocks/language-select/language-select.js
@@ -2,6 +2,10 @@ $('.language-select').click(function(){
   $(this).toggleClass('language-select--open');
 })
 
+$('.language-select').hover(function(){
+  $(this).toggleClass('language-select--open');
+})
+
 $('.language-select li').click(function(){
 var setLang = $('.language-select').data('location'),
     dataLangSelect = $(this).data('lang')
--- src/js/global-script.js
+++ src/js/global-script.js
@@ -22,7 +22,66 @@
 // });
 
 (function(){
-  // code
 
+  var newsSlider = new Swiper ('.news__container', {
+    preloadImages: false,
+    lazy: true,
+    watchSlidesVisibility: true,
+    spaceBetween: 20,
+    slidesPerView: 1,
+    grabCursor: true,
+    navigation: {
+      nextEl: '.btn--next',
+      prevEl: '.btn--prev',
+    },
+    breakpointsInverse: true,
+    breakpoints: {
+      // when window width is >= 768px
+      320: {
+        slidesPerView: 1
+      },
+      768: {
+        slidesPerView: 2
+      },
+      992: {
+        slidesPerView: 3
+      }
+    }
+  });
+
+  var partnersSlider = new Swiper ('.partners__container', {
+    preloadImages: false,
+    lazy: true,
+    watchSlidesVisibility: true,
+    spaceBetween: 20,
+    slidesPerView: 2,
+    grabCursor: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'progressbar',
+    },
+    breakpointsInverse: true,
+    breakpoints: {
+      // when window width is >= 768px
+      768: {
+        slidesPerView: 4
+      },
+      992: {
+        slidesPerView: 5
+      },
+      1280: {
+        slidesPerView: 6
+      }
+    }
+  });
 
 }());
+
+(function(){
+  // code
+  var bLazy = new Blazy({
+    selector: '.b-lazy'
+  });
+
+}());
+
